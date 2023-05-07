8a2e1e81f9cc91763b331f270d306d2ab60b963a
Edvard Englund <edvard.englund@medsci.uu.se>
2023-05-07 12:55:37 +0200

    Add definitions file

--- pension_calc/reporter.py
+++ pension_calc/reporter.py
@@ -1,11 +1,10 @@
 import os
-import pension_calc
+from pension_calc.definitions import PACKAGE_DIR
 
 
 class Reporter:
     @staticmethod
     def generate(file_name):
-        base = os.path.dirname(pension_calc.__file__)
-        path= os.path.join(base, "../output", file_name)
+        path= os.path.join(PACKAGE_DIR, "output", file_name)
         with open(path, "w", encoding="utf-8") as f:
             f.write("hej\n")
